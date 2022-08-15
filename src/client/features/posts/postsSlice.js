c95abd33c45dd410a6d723a9ee9b950332786469
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-08-15 21:42:30 +0100

    Add getPosts and addNewPosts reducer functions

--- src/client/features/posts/postsSlice.js
+++ src/client/features/posts/postsSlice.js
@@ -1,19 +1,49 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchPosts, createPost } from '../../api/index.js';
+
+export const getPosts = createAsyncThunk('posts/fetchPosts', async () => {
+	const response = await fetchPosts();
+	return response.data;
+});
+
+export const addNewPost = createAsyncThunk(
+	'posts/createPosts',
+	// The payload creator receives the partial `{creator, title, message, tags:[], selectedFile}`
+	async (initialPost) => {
+		// save the new post to the database
+		const response = await createPost(initialPost);
+		// The response from the database will include the complete object and assigned unique ID's
+		return response.data;
+	},
+);
 
 const initialState = {
-	creator: '',
-	title: '',
-	message: '',
-	tags: [],
-	selectedFile: '',
+	posts: [],
 	isLoading: true,
+	error: null,
 };
 
 const postsSlice = createSlice({
-	name: 'post',
+	name: 'posts',
 	initialState,
-	reducers: {
-
+	reducers: {},
+	extraReducers(builder) {
+		builder
+			.addCase(getPosts.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getPosts.fulfilled, (state, action) => {
+				state.isLoading = false;
+				// Add any fetched posts to the array
+				state.posts.concat(action.payload);
+			})
+			.addCase(getPosts.rejected, (state, action) => {
+				state.isLoading = false;
+				state.error = action.error.message;
+			})
+			.addCase(addNewPost.fulfilled, (state, action) => {
+				state.posts.push(action.payload);
+			});
 	},
 });
 console.log(postsSlice);
